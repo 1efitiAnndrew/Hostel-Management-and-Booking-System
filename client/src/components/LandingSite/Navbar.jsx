0ac0ee199562e16e36a272295c177213c4b259a1
Bwire Melvin Masinde <melvinmasinde4@gmail.com>
2025-11-12 11:00:59 -0800

    updated navbar.jsx

--- client/src/components/LandingSite/Navbar.jsx
+++ client/src/components/LandingSite/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar(){
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
